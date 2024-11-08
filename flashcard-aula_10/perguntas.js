5c9b4e4cf3f85d43a24f2cb9d9670a9a7124cbbb
matheuscostaleite <141827867+matheuscostaleite@users.noreply.github.com>
2024-11-08 08:46:36 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'meio ambiente',
+    'O que é fotossíntese??',
+    'A fotossíntese é o processo pelo qual as plantas produzem seu próprio alimento usando luz solar, água e dióxido de carbono, liberando oxigênio como subproduto.    '
+)
+
+criaCartao(
+    'corpo humano',
+    'Qual é a função do coração?',
+    'O coração bombeia sangue por todo o corpo, fornecendo oxigênio e nutrientes às células e removendo resíduos.    '
+)
+
+criaCartao(
+    'química',
+    'O que é um átomo?',
+    'O átomo é a menor unidade básica da matéria, composto por prótons, nêutrons e elétrons.'
+)
+
+criaCartao(
+    'falando ingles',
+    'Como se diz bom dia em ingles?',
+    'Bom dia em ingles é Good morning.'
+)
